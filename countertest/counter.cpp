0d51286f1359611701d74cf50f9f1e179baa3d0f
nirakka <ikoa.syo@gmail.com>
2019-11-27 15:54:11 +0900

    add counterarray method

--- countertest/counter.cpp
+++ countertest/counter.cpp
@@ -3,6 +3,10 @@
 #include "counter.h"
 
 // 暫定
+// return 1 => currentが0なのでをpresetに更新
+// return 0 => countdownだけ行う,
+//             currentの値がおかしければpresetの値を入れる
+// return -1=> presetが0なので何もしない
 int countdown(Counter& c)
 {
 
@@ -20,7 +24,15 @@ int countdown(Counter& c)
 	return (f3)?1:0;
 }
 
-int countdownarray(CounterArray& c)
+// result 0 => どっちもca内の値がすべて1以外
+// result 1 => 一つ目のセンサのcurrentが1
+// result 2 => 二つ目のセンサのcurrentが1
+// result 3 => 両方のセンサのcurrentが1
+int countdownarray(CounterArray& ca)
 {
-	return 0;
+	int result = 0;
+	if (countdown(ca[0])==1) result |= 1;
+	if (countdown(ca[1])==1) result |= 2;
+
+	return result;
 }
