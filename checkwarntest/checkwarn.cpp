42d5e0d40345d5f9280733451a8f17716f6abb73
nirakka <ikoa.syo@gmail.com>
2019-11-27 14:52:22 +0900

    refactor dir name

--- checkwarntest/checkwarn.cpp
+++ checkwarntest/checkwarn.cpp
@@ -26,25 +26,19 @@ int checkwarn(const Warn& inwarn, const RawValue& invalue)
 	}
 }
 
-std::string[3] messages = ["a","b","c"];
+std::string messages[] = {"a","b","c"};
 
 // 使い方
 //
 int writeWarn(int errno, const RawValue& invalue, WarnValueArray& outvaluearray)
 {
-	int16_t sv = invalue.getv16(0);
-	
-	int16_t arg=inwarn.getSensorArg;
-	uint16_t num=inwarn.getSensorNo;
-    uint16_t A_num=inwarn.getActuatorNo;
-    int16_t A_arg=inwarn.getSensorArg;
 	/*Action *a = inwarn.getAction();
 	Condition *c = inwarn.getCondition();
 	int16_t a=c->arg;
 	uint16_t n=c->no*/
 	if (errno > 0){
-		std::string str = messages[errno];
-		outvalue.message = str;
+		//std::string str = messages[errno];
+		//outvalue.message = str;
 		//(int sn,int sv,int an,int av)
 	} else {
 //		outvalueを生成
